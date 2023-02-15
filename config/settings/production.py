9ea312c1f5d1ef66273df6e3e022725033c5477e
sbkubric <sbkubric@gmail.com>
2023-02-15 17:34:28 +0300

    Added stopping of services before infra update

--- config/settings/production.py
+++ config/settings/production.py
@@ -86,7 +86,10 @@ AWS_S3_MAX_MEMORY_SIZE = env.int(
 AWS_S3_REGION_NAME = env("DJANGO_AWS_S3_REGION_NAME", default=None)
 # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#cloudfront
 AWS_S3_CUSTOM_DOMAIN = env("DJANGO_AWS_S3_CUSTOM_DOMAIN", default=None)
-aws_s3_domain = AWS_S3_CUSTOM_DOMAIN or f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+aws_s3_domain = f"{AWS_STORAGE_BUCKET_NAME}.{AWS_S3_REGION_NAME}.digitaloceanspaces.com"
+AWS_S3_ENDPOINT_URL = f"https://{aws_s3_domain}"
+AWS_DEFAULT_ACL = "public-read"
+
 # STATIC
 # ------------------------
 STATICFILES_STORAGE = "supervacancies.utils.storages.StaticRootS3Boto3Storage"
@@ -102,7 +105,7 @@ MEDIA_URL = f"https://{aws_s3_domain}/media/"
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
     "DJANGO_DEFAULT_FROM_EMAIL",
-    default="SuperVacancies <noreply@example.com>",
+    default="SuperVacancies <noreply@supervacancies.sbkubric.com>",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
