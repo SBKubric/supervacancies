607383aed80d7f0f33cd8cf31b90c1bc951547de
sbkubric <sbkubric@gmail.com>
2023-02-15 18:09:48 +0300

    Enabled CDN

--- config/settings/production.py
+++ config/settings/production.py
@@ -85,7 +85,7 @@ AWS_S3_MAX_MEMORY_SIZE = env.int(
 # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
 AWS_S3_REGION_NAME = env("DJANGO_AWS_S3_REGION_NAME", default=None)
 # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#cloudfront
-AWS_S3_CUSTOM_DOMAIN = env("DJANGO_AWS_S3_CUSTOM_DOMAIN", default=None)
+AWS_S3_CUSTOM_DOMAIN = env("DJANGO_AWS_S3_CUSTOM_CDN", default=None)
 aws_s3_domain = f"{AWS_STORAGE_BUCKET_NAME}.{AWS_S3_REGION_NAME}.digitaloceanspaces.com"
 AWS_S3_ENDPOINT_URL = f"https://{aws_s3_domain}"
 AWS_DEFAULT_ACL = "public-read"
